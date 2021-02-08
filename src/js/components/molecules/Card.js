2a9f61c0d54ef9b9a0c19b81aeadd6ab6ca339b5
Giliam <verheide.giliam+minorweb@gmail.com>
2021-02-09 00:19:16 +0100

    Updated card component

--- src/js/components/molecules/Card.js
+++ src/js/components/molecules/Card.js
@@ -1,8 +1,32 @@
-function Card(data) {
-  return `<li>
-    <h3>${data.title}</h3>
-    <p>${data.release_date}</p>
-  </li>`
+function Card({
+  data,
+  options = {}
+}) {
+  const {
+    id,
+    title,
+    release_date,
+    backdrop_path,
+    media_type
+  } = data
+
+  const {
+    link
+  } = options
+
+  return `
+  <div
+    class="card"
+    data-id="${id}"
+  >
+    ${link ? `<a href="#/${media_type}/${link}">` : ''}
+      <h3>${title}</h3>
+      <p>${release_date}</p>
+      <div class="card__background">
+        <img src="https://image.tmdb.org/t/p/original${backdrop_path}" alt="${title}" />
+      </div>
+    ${link ? '</a>' : ''}
+  </div>`
 }
 
 export default Card
